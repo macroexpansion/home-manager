41555f486c6d448ef4aa6a9b08f28915cc059e79
macroexpansion <trasuadev@gmail.com>
2023-11-21 19:13:38 +0700

    kitty theme

--- home.nix
+++ home.nix
@@ -93,35 +93,70 @@
         size = 14;
       };
       settings = {
-# font_family         = "Cascadia Code SemiBold";
-# bold_font           = "Cascadia Code Bold";
-# italic_font         = "Cascadia Code SemiBold";
-# bold_italic_font    = "Cascadia Code Bold";
-        disable_ligatures   = "never";
+        disable_ligatures = "never";
         cursor_shape = "block";
         shell_integration = "disabled";
-        background = "#282a36";
-        foreground = "#ffffff";
-        color0 = "#282c34";
-        color8 = "#ffffff";
-        color1 = "#be5046";
-        color9 = "#e06c75";
-        color10 = "#1cdc9a";
-        color2 = "#1abc9c";
-        color3 = "#e5c07b";
-        color11 = "#e5d660";
-        color4 = "#56b6c2";
-        color12 = "#14ffff";
-        color13 = "#cb1ed1";
-        color5 = "#c678dd";
-        color6 = "#61afef";
-        color14 = "#1a8fff";
-        color7 = "#dddddd";
-        color15 = "#ffffff";
-        tab_bar_edge = "top";
+        tab_bar_min_tabs = 1;
+        tab_bar_edge = "bottom";
         tab_bar_style = "powerline";
+        tab_powerline_style = "slanted";
+        tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
         hide_window_decorations = "yes";
         macos_option_as_alt = "yes";
+
+        foreground = "#CDD6F4";
+        background = "#1E1E2E";
+        selection_foreground = "#1E1E2E";
+        selection_background = "#F5E0DC";
+
+        color0 = "#45475A";
+        color8 = "#585B70";
+
+        color1 = "#F38BA8";
+        color9 = "#F38BA8";
+
+        color2 = "#A6E3A1";
+        color10 = "#A6E3A1";
+
+        color3 = "#F9E2AF";
+        color11 = "#F9E2AF";
+
+        color4 = "#89B4FA";
+        color12 = "#89B4FA";
+
+        color5 = "#F5C2E7";
+        color13 = "#F5C2E7";
+
+        color6 = "#94E2D5";
+        color14 = "#94E2D5";
+
+        color7 = "#BAC2DE";
+        color15 = "#A6ADC8";
+
+        cursor            = "#F5E0DC";
+        cursor_text_color = "#1E1E2E";
+
+        url_color = "#F5E0DC";
+
+        active_border_color   = "#B4BEFE";
+        inactive_border_color = "#6C7086";
+        bell_border_color     = "#F9E2AF";
+
+        wayland_titlebar_color = "system";
+        macos_titlebar_color = "system";
+
+        active_tab_foreground   = "#11111B";
+        active_tab_background   = "#CBA6F7";
+        inactive_tab_foreground = "#CDD6F4";
+        inactive_tab_background = "#181825";
+        tab_bar_background      = "#11111B";
+
+        mark1_foreground = "#1E1E2E";
+        mark1_background = "#B4BEFE";
+        mark2_foreground = "#1E1E2E";
+        mark2_background = "#CBA6F7";
+        mark3_foreground = "#1E1E2E";
+        mark3_background = "#74C7EC";
       };
   };
 
