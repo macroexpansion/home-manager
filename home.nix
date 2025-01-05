3cc7e01713eef7f2014d2d8b8581230a13552a41
macroexpansion <macroexpansion@gmail.com>
2025-01-05 18:18:58 +0700

    remove z plugin

--- home.nix
+++ home.nix
@@ -175,15 +175,6 @@
         { name = "bass"; src = pkgs.fishPlugins.bass.src; }
         { name = "autopair"; src = pkgs.fishPlugins.autopair.src; }
         { name = "git"; src = pkgs.fishPlugins.plugin-git.src; }
-        # {
-        #     name = "z";
-        #     src = pkgs.fetchFromGitHub {
-        #         owner = "jethrokuan";
-        #         repo = "z";
-        #         rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
-        #         sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
-        #     };
-        # }
       ];
       shellAliases = {
           ".." = "cd ..";
