9a90c46d3ac2975569af17a7de8adef4c48cdbd2
macroexpansion <macroexpansion@gmail.com>
2025-03-08 18:01:00 +0700

    add arch + yazi

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "archuser";
-  home.homeDirectory = "/Users/archuser";
+  home.homeDirectory = "/home/archuser";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -13,7 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -34,6 +34,7 @@
     pkgs.zig
     pkgs.jujutsu
     pkgs.kubernetes-helm
+    pkgs.yazi
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
