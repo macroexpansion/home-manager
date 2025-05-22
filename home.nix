ff3fcf8b07053b095baaa456cf7717dbc18434d2
macroexpansion <macroexpansion@gmail.com>
2025-05-23 01:11:22 +0700

    fish_cursor_insert block

--- home.nix
+++ home.nix
@@ -174,6 +174,7 @@
         set -g theme_hide_hostname no
         set -g theme_hostname always
         set -g fish_key_bindings fish_vi_key_bindings # Set Vi-mode key bindings
+        set -g fish_cursor_insert block
 
         zoxide init fish | source
       '';
